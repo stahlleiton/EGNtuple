54c5abe492ba04869c0e5b8d329da56bb5b7a3c2
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2024-08-17 16:43:16 +0200

    Adapted code for Run3 UPC

---
+++ TrigNtup/test/egRegTreeMaker_UPC.py
@@ -0,0 +1,91 @@
+isCrabJob=False #script seds this if its a crab job
+
+# Import configurations
+import FWCore.ParameterSet.Config as cms
+import os
+import sys
+# set up process
+from Configuration.Eras.Era_Run3_2023_UPC_cff import Run3_2023_UPC
+process = cms.Process("HEEP",Run3_2023_UPC)
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing ('analysis') 
+options.register('isMC',True,options.multiplicity.singleton,options.varType.bool," whether we are running on MC or not")
+options.parseArguments()
+
+print(options.inputFiles)
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(options.inputFiles),  
+                          )
+
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32(5000),
+    limit = cms.untracked.int32(10000000)
+)
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+#Load geometry
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.autoCond import autoCond
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+#gt doesnt really matter much as no reco but nice to get it right
+if options.isMC:
+    process.GlobalTag = GlobalTag(process.GlobalTag, '132X_mcRun3_2023_realistic_HI_v10', '')
+else:
+    process.GlobalTag = GlobalTag(process.GlobalTag, '132X_dataRun3_Prompt_v7', '')
+
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Geometry.CaloEventSetup.CaloTowerConstituents_cfi")
+process.load("Configuration.StandardSequences.Services_cff")
+
+# set the number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string(options.outputFile)
+)
+
+process.egRegTreeMaker = cms.EDAnalyzer("EGRegTreeMaker",
+                                        verticesTag = cms.InputTag("offlinePrimaryVertices"),
+                                        rhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
+                                        genPartsTag = cms.InputTag("genParticles"),
+                                        puSumTag = cms.InputTag("addPileupInfo"),
+                                     #   scTag = cms.VInputTag("particleFlowSuperClusterECAL:particleFlowSuperClusterECALBarrel","particleFlowSuperClusterECAL:particleFlowSuperClusterECALEndcapWithPreshower"),
+                                        scTag = cms.VInputTag("particleFlowEGamma",),
+                                        scAltTag = cms.VInputTag("particleFlowSuperClusterECALNoThres:particleFlowSuperClusterECALBarrel","particleFlowSuperClusterECALNoThres:particleFlowSuperClusterECALEndcapWithPreshower"),
+                                        ecalHitsEBTag = cms.InputTag("reducedEcalRecHitsEB"),
+                                        ecalHitsEETag = cms.InputTag("reducedEcalRecHitsEE"),
+                                        elesTag = cms.InputTag("gedGsfElectrons"),
+                                        phosTag = cms.InputTag("gedPhotons"),
+                                        elesAltTag = cms.VInputTag(),
+                                        phosAltTag = cms.VInputTag(),
+                                        )
+
+process.load("SHarper.TrigNtup.rePFSuperCluster_cff")
+
+
+process.p = cms.Path(process.rePFSuperClusterThresSeq*process.egRegTreeMaker)
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('AODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    fileName = cms.untracked.string(options.outputFile.replace(".root","_EDM.root")),
+    outputCommands = cms.untracked.vstring('drop *',
+                                           "keep *_*_*_HEEP",
+                                    )                                           
+                                   )
+#process.out = cms.EndPath(process.AODSIMoutput)
